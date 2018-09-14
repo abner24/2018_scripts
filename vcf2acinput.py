3c221c35fca5b0065021349906fc40de30e55aca
Abner Lim <kalom_ti@hotmail.com>
2018-09-14 17:46:37 +0800

    initialised repo

---
+++ vcf2acinput.py
@@ -0,0 +1,19 @@
+#!/usr/env/bin/ python3
+
+###
+# Mandatory input fields include
+# 1. Chromosome
+# 2. Start
+# 3. End
+# 4. Reference
+# 5. Alternate
+###
+
+import pandas as pd
+import io
+import argparse
+
+def main(caller, vcf_file):
+
+if __name__ == '__main__':
+
